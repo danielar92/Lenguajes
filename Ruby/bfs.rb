1b82310aa92fe036df16c64e989813b73cd991f3
prengifo <patrick.rengifo@gmail.com>
2014-06-16 21:31:54 -0430

    almost ready bfs. missing predicates, actions, and bibles

--- Ruby/bfs.rb
+++ Ruby/bfs.rb
@@ -5,17 +5,18 @@
 # Recorrido BFS en un comportamiento
 
 module Bfs
+  # Recorrido BFS para encontrar un nodo que cumpla predicate
   def find(start, predicate)
     # Conjunto de nodos visitados
     nodes = {start.value => false}
     
     # Creamos la pila de nodos
-    items        = [start]
+    items = [start]
     nodes[start.value] = true
     
     while item = items.delete_at(0)
       # Si es el nodo cumple con el predicado, lo retornamos.
-      return item if item.predicate?
+      return item if item.value == predicate
       
       # Sino buscamos sus adyacentes y los empilamos.
       item.each(1) do |x| 
@@ -28,19 +29,23 @@ module Bfs
     # Si no encontramos nada
     return nil
   end
+  
+  # Recorrido BFS que devuelve el camino desde la raiz hasta el nodo que cumpla
+  # el predicate
   def path(start, predicate)
     # Conjunto de nodos visitados
     nodes = {start.value => false}
     # Camino vacio
-    path = [start]
+    path = []
     
     # Creamos la pila de nodos
-    items        = [start]
+    items = [start]
     nodes[start.value] = true
     
     while item = items.delete_at(0)
+      path.push(item.value)
       # Si es el nodo cumple con el predicado, lo retornamos.
-      return item if item.predicate?
+      return path if item.value == predicate
       
       # Sino buscamos sus adyacentes y los empilamos.
       item.each(1) do |x| 
@@ -53,7 +58,34 @@ module Bfs
     # Si no encontramos nada
     return nil
   end
-  def walk(start, predicate)
+  
+  # Recorrido BFS que recorre en su totalidad el espacio de busqueda ejecutando
+  # el action en cada nodo y retornando la lista de nodos recorridos. Si se 
+  # omite action, solo devuelve la lista de nodos recorridos.
+  def walk(start, action)
+    # Conjunto de nodos visitados
+    nodes = {start.value => false}
+    # Camino vacio
+    path = []
+    
+    # Creamos la pila de nodos
+    items = [start]
+    nodes[start.value] = true
+    
+    while item = items.delete_at(0)
+      path.push(item.value)
+      #aplicar el action
+      
+      # Sino buscamos sus adyacentes y los empilamos.
+      item.each(1) do |x| 
+        unless nodes[x.value] 
+          nodes[x.value] = true 
+          items.push(x)
+        end 
+      end
+    end
+    # Si no encontramos nada
+    return path
   end
 end
 
@@ -76,11 +108,6 @@ class BinTree
   end
 end
 
-b = BinTree.new(5,BinTree.new(4, BinTree.new(3)))
-# b.each(1) {|x| puts x.value}
-res = b.find(b,7)
-puts res.value unless res.nil?
-
 class GraphNode
   include Bfs
   attr_accessor :value,     # Valor alamacenado en el nodo
@@ -94,11 +121,6 @@ class GraphNode
     @children.each {|x| yield x } unless @children.nil?
   end
 end
-
-g = GraphNode.new(4, [GraphNode.new(3), GraphNode.new(2)])
-# g.each(1) {|x| puts x.value}
-res = g.find(g,8)
-puts res.value unless res.nil?
 
 # Arboles Implicitos
 
@@ -115,3 +137,15 @@ def LCR
     # Su codigo aqui
   end
 end
+
+b = BinTree.new(5,BinTree.new(4, BinTree.new(3)))
+# b.each(1) {|x| puts x.value}
+res = b.find(b,4)
+puts res.value unless res.nil?
+puts b.path(b,3)
+puts b.walk(b,3)
+
+g = GraphNode.new(4, [GraphNode.new(3), GraphNode.new(2)])
+# g.each(1) {|x| puts x.value}
+res = g.find(g,8)
+puts res.value unless res.nil?
